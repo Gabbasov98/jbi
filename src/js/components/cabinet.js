27cbb1550f40da0fc2e50d3e9261cf8bbff5a080
Marsel Gabbasov <marsel13_98@mail.ru>
2022-10-31 08:23:48 +0500

    added doc-modal

--- src/js/components/cabinet.js
+++ src/js/components/cabinet.js
@@ -2,3 +2,17 @@ $(".order-card__toggle").click(function () {
     $(this).toggleClass("order-card__toggle--active")
     $(this).parents(".order-card").find(".order-card__hidden").slideToggle()
 })
+
+$(".doc-card__check input").change(function () {
+    let checkedInputNum = $(".doc-card__check input:checked").length
+    $(".doc-selected__num").html(checkedInputNum)
+    if(checkedInputNum > 0){
+        $(".doc-selected").addClass("doc-selected--active")
+    } else{
+        $(".doc-selected").removeClass("doc-selected--active")
+    }
+})
+
+$(".doc-selected__close").click(function () {
+    $(".doc-selected").removeClass("doc-selected--active")
+})
